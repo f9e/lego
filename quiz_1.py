a7efb2dcd5ff8fe993fc236fd2037067bab03512
f9e <35344456+f9e@users.noreply.github.com>
2024-09-04 14:54:59 -0500

    Create quiz_1.py

---
+++ quiz_1.py
@@ -0,0 +1,23 @@
+print(2+2)
+
+print(2*2/8)
+
+print(87%5)
+
+print(2*2)
+
+print(2**2)
+
+print("2"*4)
+
+print(len("2"*8))
+
+print("two"[2])
+
+def greet(i):
+    print("hello " + i)
+
+
+greet("world")
+
+   print("okay?")
