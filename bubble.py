ce41a43434a2716b2c35671ca2af00eb96ca3c25
f9e <35344456+f9e@users.noreply.github.com>
2024-09-04 14:55:18 -0500

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,36 +1,52 @@
 from hub import light_matrix
 from hub import motion_sensor
 import time
-import random
 import runloop
 
 rows, cols = (5, 5)
 
+
+## If the pixel is out of range
+#loop it around the screen
+def checkRange(pos):
+    if pos > 4:
+        pos -= 5
+    if pos < 0:
+        pos += 5
+    return pos
+
 async def main():
 
-  pos = 12
+  xPos , yPos = (2,2)
   while True:
 
     # Move the pixel depending on the up face
-    face =  motion_sensor.up_face()
+    face = motion_sensor.up_face()
     if face == motion_sensor.FRONT:
-      pos+=5
+      xPos += 1
     if face == motion_sensor.BACK:
-      pos-=5
+      xPos -= 1
     if face == motion_sensor.RIGHT:
-      pos+=1
+      yPos += 1
     if face == motion_sensor.LEFT:
-      pos-=1      
-    if pos > 24:
-        pos -= 25
-    if pos < 0:
-        pos += 25
+      yPos -= 1
 
-    # set the array to be empty
-    a = [[0 for i in range(cols)] for j in range(rows)]
+    # If the pixel goes off the board, replace it
+    xPos = checkRange(xPos)
+    yPos = checkRange(yPos)
 
-    # map the pixel based on the number
-    a[int(pos%5)][int(pos/5)] = True
+    # set all pixels in the array to false.
+    # a = [
+    #    [0,0,0,0,0],
+    #    [0,0,0,0,0],
+    #    [0,0,0,0,0],
+    #    [0,0,0,0,0],
+    #    [0,0,0,0,0],
+    # ]
+    a = [[False for i in range(cols)] for j in range(rows)]
+
+    # set the "on" pixel
+    a[int(yPos)][int(xPos)] = True
 
     # update the light matrix
     for i in range(5):
@@ -38,6 +54,6 @@ async def main():
             light_matrix.set_pixel(i,j, 100 if a[i][j] else 0 )
 
     # Wait for 1/10th of a second
-    time.sleep_ms(100)
+    time.sleep_ms(200)
 
 runloop.run(main())
